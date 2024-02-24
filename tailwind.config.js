f581ad6a1c4f53196ab63038c20527efef411662
wandaem <wanshade@gmail.com>
2024-02-24 16:56:25 +0800

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Adjust this line as necessary to include any file paths where you'll use Tailwind classes
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
